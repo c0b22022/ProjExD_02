18ff92ef58abb33a469ec8e4a18ca8ee5cf3abfa
C0b22022 <c0b2202295@edu.teu.ac.jp>
2023-06-20 15:04:12 +0900

    問題2 3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,7 +4,12 @@ import pygame as pg
 
 
 WIDTH, HEIGHT = 1600, 900
-
+delta = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+}
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -12,6 +17,8 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900, 400
     bm_img = pg.Surface((20,20))
     pg.draw.circle(bm_img,(255,0,0),(10,10),10)
     x = random.randint(0,WIDTH)
@@ -19,19 +26,28 @@ def main():
     bm_rct = bm_img.get_rect() #爆弾Surfaceを抽出する
     bm_rct.center = x, y       #爆弾の初期座標
     bm_img.set_colorkey((0,0,0))#円を囲う四角の四隅を透明に
+    vx, vy = +5, +5
     clock = pg.time.Clock()
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]
+        for k, mv in delta.items():
+            if key_lst[k]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        kk_rct.move_ip(sum_mv)
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        screen.blit(kk_img, kk_rct)
+        bm_rct.move_ip(vx,vy)
         screen.blit(bm_img,bm_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
