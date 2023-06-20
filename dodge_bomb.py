f408ce0fa807380e46b8e166b77103c666ba3c43
C0b22022 <c0b2202295@edu.teu.ac.jp>
2023-06-20 14:22:35 +0900

    問題１

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import pygame as pg
 
@@ -11,6 +12,13 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bm_img = pg.Surface((20,20))
+    pg.draw.circle(bm_img,(255,0,0),(10,10),10)
+    x = random.randint(0,WIDTH)
+    y = random.randint(0,HEIGHT)
+    bm_rct = bm_img.get_rect() #爆弾Surfaceを抽出する
+    bm_rct.center = x, y       #爆弾の初期座標
+    bm_img.set_colorkey((0,0,0))#円を囲う四角の四隅を透明に
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -20,6 +28,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bm_img,bm_rct)
         pg.display.update()
         tmr += 1
         clock.tick(10)
